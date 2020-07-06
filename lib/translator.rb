11a552ff7df9a595534048eceaf3779a86beabd2
Gregory A.S. Wright <geslim@gmail.com>
2020-07-06 13:07:39 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
-# require modules here
+require 'pry'
+require 'yaml'
+path = YAML.load_file('lib/emoticons.yml')
 
-def load_library
-  # code goes here
+
+
+def load_library (path)
+  emoticons = YAML.load_file('lib/emoticons.yml')
+    library = {}
+  emoticons.each do |emotion, expression|
+    english_expression = expression[0]
+    japanese_expression = expression[1]
+    library[emotion] = expression
+
+  end
+  library
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+def get_japanese_emoticon(path, library)
+  load_library.each do |path|
+
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, library)
+  load_library
 end
